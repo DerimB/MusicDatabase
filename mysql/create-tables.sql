ca6b3d76f5cf721370d9fd1eded82eb37ee8ef6b
DerimB <123917611+DerimB@users.noreply.github.com>
2024-02-04 00:51:03 -0500

    Add files via upload

---
+++ mysql/create-tables.sql
@@ -0,0 +1,39 @@
+
+-- Create a table to store information about artists
+CREATE TABLE IF NOT EXISTS artist (
+  artist_id   INT          NOT NULL AUTO_INCREMENT,
+  artist_name VARCHAR(128) NOT NULL,
+  PRIMARY KEY (artist_id),
+  INDEX index_artist_id (artist_id)
+);
+
+-- Create a table to store information about albums
+CREATE TABLE IF NOT EXISTS album (
+  artist_id  INT          NOT NULL,
+  album_id   INT          NOT NULL AUTO_INCREMENT,
+  album_name VARCHAR(128) NOT NULL,
+  PRIMARY KEY (album_id),
+  FOREIGN KEY (artist_id) REFERENCES artist(artist_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  INDEX index_album_id (album_id)
+);
+
+-- Create a table to store information about tracks
+CREATE TABLE IF NOT EXISTS track (
+  album_id   INT          NOT NULL,
+  track_id   INT          NOT NULL,
+  track_name VARCHAR(128) NOT NULL,
+  time       DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (track_id),
+  FOREIGN KEY (album_id) REFERENCES album(album_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  INDEX index_track_id (track_id)
+);
+
+-- Create a table to store information about played tracks
+CREATE TABLE IF NOT EXISTS played (
+  played     TIMESTAMP    NOT NULL,
+  track_id   INT          NOT NULL,
+  PRIMARY KEY (played),
+  FOREIGN KEY (track_id) REFERENCES track(track_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  INDEX index_played (played)
+);
+
---
+++ mysql/setup.sql
@@ -0,0 +1,9 @@
+
+DROP DATABASE IF EXISTS music;
+
+CREATE DATABASE music;
+
+USE music;
+
+source create-tables.sql;
+source insert-values.sql;
